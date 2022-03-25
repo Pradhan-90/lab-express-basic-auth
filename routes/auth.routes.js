78ccf37a82c359df29fc9e0e72461892f2510682
Akanksha Singh Pradhan <akanksha_singh15@yahoo.com>
2022-03-25 11:53:25 +0100

    second iteration done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -39,4 +39,32 @@ router.post("/signup", async (req, res, next) => {
   }
 });
 
+router.post("/login", async (req, res, next) => {
+  const userInput = req.body;
+  if (!userInput.username) {
+    res.render("auth/login", { errorMessage: "Enter Username" });
+    return;
+  } else if (userInput.password?.length < 8) {
+    res.render("auth/login", {
+      errorMessage: "Password should be more than 8 digits",
+    });
+    return;
+  }
+  const userExist = await User.findOne({ username: userInput.username });
+  if (!userExist) {
+    res.render("auth/login", { errorMessage: "Username does not exist" });
+    return;
+  }
+  const passwordValid = await bcryptjs.compare(
+    userInput.password,
+    userExist.password
+  );
+  if (!passwordValid) {
+    res.render("auth/login", { errorMessage: "Invalid Password" });
+    return;
+  }
+  req.session.user = userExist;
+  res.redirect("/");
+});
+
 module.exports = router;
